1cea13949c6fe8d940dc853899d7d508d01735e7
BorkovskayaAnna <45067969+BorkovskayaAnna@users.noreply.github.com>
2021-06-02 17:21:33 +0300

    Revert "review 1"

--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
+import reportWebVitals from './reportWebVitals'
 
-import style from "./styles/index.scss";
+import style from './styles/index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
